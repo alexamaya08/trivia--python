32fd3f70309833de6e2357913901ad729ca5e256
Joseph Alexander Alexander <86429800+alexamaya08@users.noreply.github.com>
2022-09-16 00:40:13 +0000

    SE IMPLEMENTO COMANDO OS

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 BLACK   = '\033[30m'
-ROJO     = '\033[31m'
+ROJO  = '\033[31m'
 VERDE   = '\033[32m'
 AMARILLO  = '\033[33m'
-AZUL= '\033[34m'
+AZUL  = '\033[34m'
 MAGENTA = '\033[35m'
-CYAN    = '\033[36m'
-BLANCO   = '\033[37m'
-RESET   = '\033[39m'
+CYAN  = '\033[36m'
+BLANCO  = '\033[37m'
+RESET  = '\033[39m'
 
 import random
 import time
@@ -33,7 +33,7 @@ while reinicio_trivia==True:
       print(CYAN+"ya!"+RESET)
       time.sleep(1)
     elif numero_carga == 5:
-        print(AMARILLO+"PREPARADO?"+RESET)
+        print(AMARILLO+"¿PREPARADO?"+RESET)
         time.sleep(1)
     else:
       print(numero_carga)
@@ -59,6 +59,8 @@ while reinicio_trivia==True:
       incorrecta += 1
   elif respuesta_1 == "c":
       print(ROJO+"Incorrecto!", nombre, "Intenta de nuevo"+RESET)
+      puntaje -= random.randint(1, 5)
+      incorrecta += 1
   elif respuesta_1 == "d":
       print(ROJO+"Incorrecto!", nombre, "Ojo, estamos en el bicentenario"+RESET)
       puntaje -= random.randint(1, 5)
@@ -84,12 +86,26 @@ while reinicio_trivia==True:
   
   if respuesta_2 == "a":
     print("Incorrecto!", nombre, "estuviste cerca")
+    puntaje -= random.randint(1, 5)
+    incorrecta += 1
   elif respuesta_2 == "c":
     print("Incorrecto!", nombre, "son muchos litros")
+    puntaje -= random.randint(1, 5)
+    incorrecta += 1
   elif respuesta_2 == "d":
     print("Incorrecto!", nombre, "tenemos casi la mitad")
+    puntaje -= random.randint(1, 5)
+    incorrecta += 1
   else:
     print("Muy bien\033[34m", nombre, "\033[39m!")
+    puntaje += random.randint(5, 10)
+    correcta += 1
   time.sleep(1)
+  print(VERDE+nombre, "llevas", puntaje, "puntos." + RESET)
+  time.sleep(2)
+  input("Presione enter para continuar")
   os.system('clear')
- 
+  print("\nCantidad de respuestas correctas: ", correcta)
+  time.sleep(1)
+  print("Cantidad de respuestas incorrectas: ", incorrecta)
+  time.sleep(1)
